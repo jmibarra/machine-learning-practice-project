c6fbd14807db0ed94a82cabacbba0e00998c3952
Juan Ibarra <jmibarra86@gmail.com>
2023-03-09 18:19:19 -0300

    Implementacion del algoritmo KNN

---
+++ Algoritmo KNN/knntest.py
@@ -0,0 +1,32 @@
+# conda activate ml_practice_env
+# https://www.youtube.com/watch?v=ngLyX54e1LU&list=PLqnslRFeH2Upcrywf-u2etjdxxkL8nl7E
+from knn import KNN
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234)
+
+# print(X_train.shape)
+# print(X_train[0])
+
+# print(y_train.shape)
+# print(y_train)
+
+# plt.figure()
+# plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, edgecolor='k', s=20)
+# plt.show()
+
+clf = KNN(k=3)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+accuracy = np.sum(predictions == y_test) / len(y_test)
+print(accuracy)
